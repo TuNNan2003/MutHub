fbecd26bc1301e7baa5d8e66245379cdc0584b27
wuuhuua <wuuhuua@163.com>
2023-12-25 18:02:04 +0800

    <init> page init

---
+++ FrontEnd/muthub/src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('../view/MainPage.vue')
+  }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router;
